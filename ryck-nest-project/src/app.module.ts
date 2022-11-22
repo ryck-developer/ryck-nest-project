d5fb416e2790603567738ef1fd2317aec3b0f9b6
luiz-henrique-chaves <88977217+luiz-henrique-chaves@users.noreply.github.com>
2022-11-22 16:06:00 -0300

    3 files modified, 11 added, 9 deleted

--- ryck-nest-project/src/app.module.ts
+++ ryck-nest-project/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
